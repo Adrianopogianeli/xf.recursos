080a5f7f138138882cab96ce86d9e4faef6374f7
logonrm <logonrm@fiap.com.br>
2018-05-21 20:26:39 -0300

    Add project files.

---
+++ XF.Recursos/XF.Recursos/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XF.Recursos
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        #region Estilo
+        private async void btnSimples_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.SimplesView());
+        }
+        private async void btnGlobal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.GeralView());
+        }
+        private async void btnDinamico_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.DinamicoView());
+        }
+        private async void btnTriggers_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Estilo.TriggersView());
+        }
+        #endregion
+        #region Lista
+        private async void btnListSimples_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Lista.SimplesView());
+        }
+        #endregion
+        private async void btnListClasse_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Lista.ClasseView());
+        }
+    }
+}
